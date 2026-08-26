e7df284b802d31cfb1acfa0497f0b14d932839f6
juanlealr <juan.leal04@uptc.edu.co>
2026-08-25 20:49:34 -0500

    feat: demonstrate IoC with constructor-injected SaludoInstitucionalService

---
+++ src/main/java/co/edu/uptc/rica/ArranqueInformativo.java
@@ -0,0 +1,20 @@
+package co.edu.uptc.rica;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArranqueInformativo implements CommandLineRunner {
+
+    private final SaludoInstitucionalService saludoInstitucionalService;
+
+    public ArranqueInformativo(SaludoInstitucionalService saludoInstitucionalService) {
+        this.saludoInstitucionalService = saludoInstitucionalService;
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println(saludoInstitucionalService.mensajeDeBienvenida());
+    }
+
+}
